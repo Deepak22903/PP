d841b31e63f6835ef58b211f84566daae63467a7
Deepak22903 <deepakshitole4@gmail.com>
2025-02-13 00:32:01 +0530

    Updated

--- fb_poster/include/header.h
+++ fb_poster/include/header.h
@@ -1,19 +1,28 @@
 #ifndef HEADER_H
 #define HEADER_H
-
+#include <Poco/Base64Encoder.h>
 #include <chrono>
 #include <cppcodec/base64_rfc4648.hpp>
+#include <cstddef>
 #include <ctime>
 #include <curl/curl.h>
+#include <curl/easy.h>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include <sstream>
 #include <string>
+#include <vector>
 
 using json = nlohmann::json;
 using namespace std;
 using namespace cppcodec;
+
+// Base64 encoding table
+const std::string base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                 "abcdefghijklmnopqrstuvwxyz"
+                                 "0123456789+/";
 
 // ✅ Use `const string` instead of `#define`
 const string CLOUDFLARE_ACCOUNT_ID = "d03ba4adab44b19322eed9dd5a40de7d";
@@ -26,10 +35,12 @@ const string FB_ACCESS_TOKEN =
     "LWZCndJqhcfk0Yb994fhDx980ZA3xlbw";
 
 // Function Declarations
+std::string image_to_base64(const std::string &image_path);
 string generatePrompt();
 string generateImage(string prompt);
 size_t WriteCallback(void *contents, size_t size, size_t nmemb, string *output);
 string post_to_fb(string generated_img);
 string get_date();
+string generateImageFromImage(string img, string prompt);
 
 #endif // HEADER_H
--- fb_poster/src/main.cpp
+++ fb_poster/src/main.cpp
@@ -7,9 +7,13 @@ int main() {
   cout << "Generating image..." << endl;
   string img_path = generateImage(prompt);
   cout << "Image generated successfully with path : " << img_path << endl;
-  cout << "Publishing to facebook..." << endl;
-  string post_id = post_to_fb(img_path);
-  cout << "Post added with post_id : " << post_id << endl;
+  // cout << "Publishing to facebook..." << endl;
+  // string post_id = post_to_fb(img_path);
+  // cout << "Post added with post_id : " << post_id << endl;
+  // string img =
+  //     generateImageFromImage("./output/test.jpg", "generate the similar
+  //     image");
+  // cout << "Image generated successfully at " << img << endl;
 
   return 0;
 }
