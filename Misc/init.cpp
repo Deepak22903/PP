56322e72ace3e214eaae85aa086602c399afe40b
Deepak22903 <deepakshitole4@gmail.com>
2025-02-25 18:17:46 +0530

    Updated

---
+++ Misc/init.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[]) {
+  // int x;
+  // x += 10;
+  // std::string s{std::to_string(x)};
+  // s += "Hello World!";
+  // // std::cout << s << x << "\n";
+  // std::cout << sizeof(x);
+  // int a{2};
+  // int b{5};
+  // a = a xor b;
+  // b = a xor b;
+  // a = a xor b;
+  // std::cout << a << std::endl << b << std::endl;
+  // 2 + 3;
+  int a{};
+  cout << "Enter an integer: ";
+  cin >> a;
+  cout << "Double " << a << " is : " << a * 2 << "\n";
+  cout << "Triple " << a << " is : " << a * 3 << "\n";
+
+  return 0;
+}
