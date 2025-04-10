93168c12eafb64058673fc0eb9857aa12e9974e7
Deepak22903 <deepakshitole4@gmail.com>
2025-04-10 22:58:39 +0530

    Updated

---
+++ Misc/cpp_snip.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  cout <<
+
+      "#include <iostream>\n"
+      "using namespace std;\n\n"
+
+      "int main (int argc, char *argv[]) {\n"
+
+      "return 0;\n"
+      "}\n";
+
+  return 0;
+}
