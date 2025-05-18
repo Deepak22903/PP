24dec7880f4b79e92eb007d940f5fe90c23f4039
Deepak22903 <deepakshitole4@gmail.com>
2025-05-18 10:58:51 +0530

    Updated

--- Misc/pascalsTriangle.cpp
+++ Misc/pascalsTriangle.cpp
@@ -7,11 +7,32 @@ class Solution {
 public:
   vector<vector<int>> generate(int numRows) {
     // Solution goes here
+    vector<vector<int>> matrix;
+    for (int i = 0; i < numRows; i++) {
+      if (matrix.size() == 0) {
+        matrix.push_back({1});
+      } else if (matrix.size() == 1) {
+        matrix.push_back({1, 1});
+      } else {
+        vector<int> curr;
+        vector<int> prev = matrix[i - 1];
+        curr.push_back(1);
+        // sliding window
+        for (int j = 0; j < prev.size() - 1; j++) {
+          curr.push_back(prev[j] + prev[j + 1]);
+        }
+
+        // sliding window ends
+        curr.push_back(1);
+        matrix.push_back(curr);
+      }
+    }
+    return matrix;
   }
 };
 
 int main() {
-  int numRows = 5;
+  int numRows = 30;
 
   Solution sol;
   vector<vector<int>> result = sol.generate(numRows);
