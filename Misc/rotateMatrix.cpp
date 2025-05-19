43e47230456ad0f135dce1bb7179d05824fcc57c
Deepak22903 <deepakshitole4@gmail.com>
2025-05-19 15:03:58 +0530

    Updated

---
+++ Misc/rotateMatrix.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  void rotate(vector<vector<int>> &matrix) {
+    // Solution goes here
+  }
+};
+
+int main() {
+  vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+  Solution sol;
+  sol.rotate(matrix);
+
+  // Output the rotated matrix
+  for (const auto &row : matrix) {
+    for (int val : row) {
+      cout << val << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
