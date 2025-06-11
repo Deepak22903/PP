d767d8e8364981682583d9d6ad0d664945d29acc
Deepak22903 <deepakshitole4@gmail.com>
2025-06-11 18:58:34 +0530

    Updated

--- recursion/combinationSum2.cpp
+++ recursion/combinationSum2.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -7,24 +8,28 @@ public:
   vector<vector<int>> res;
   void comb(vector<int> &candidates, int target, int sum, int i,
             vector<int> ds) {
-    if (i >= candidates.size()) {
-      return;
-    }
     if (sum == target) {
       res.push_back(ds);
       return;
     }
 
-    sum += candidates[i];
-    ds.push_back(candidates[i]);
-    comb(candidates, target, sum, i + 1, ds);
-    sum -= candidates[i];
-    ds.pop_back();
-    comb(candidates, target, sum, i + 1, ds);
+    for (int k = i; k < candidates.size(); k++) {
+      if (k > i && candidates[k] == candidates[k - 1]) {
+        continue;
+      }
+      if (sum > target)
+        break;
+      sum += candidates[k];
+      ds.push_back(candidates[k]);
+      comb(candidates, target, sum, k + 1, ds);
+      sum -= candidates[k];
+      ds.pop_back();
+    }
   }
 
   vector<vector<int>> combinationSum2(vector<int> &candidates, int target) {
     // Implementation goes here
+    sort(candidates.begin(), candidates.end());
     comb(candidates, target, 0, 0, {});
     return res;
   }
