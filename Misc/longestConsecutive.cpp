0cf2b9cb55abd9225e3dbeb462c13fa5cca0c630
Deepak22903 <deepakshitole4@gmail.com>
2025-09-16 13:55:22 +0530

    Updated

---
+++ Misc/longestConsecutive.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  int longestConsecutive(vector<int> &nums) {
+    if (nums.empty() || nums.size() == 1)
+      return 0;
+    sort(nums.begin(), nums.end());
+    int maxi = 0;
+    int i = 0;
+    int j = 0;
+    while (i < nums.size()) {
+      while (i < nums.size() - 1 && abs(abs(nums[i]) - abs(nums[i + 1]))) {
+        i++;
+      }
+      maxi = max(maxi, i - j + 1);
+      i++;
+      j = i;
+    }
+    return maxi;
+  }
+};
+
+int main() {
+  Solution sol;
+
+  vector<int> nums1 = {100, 4, 200, 1, 3, 2};
+  cout << "Test 1: " << sol.longestConsecutive(nums1) << "\n";
+  // Expected: 4 (sequence: 1, 2, 3, 4)
+
+  vector<int> nums2 = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+  cout << "Test 2: " << sol.longestConsecutive(nums2) << "\n";
+  // Expected: 9 (sequence: 0,1,2,3,4,5,6,7,8)
+
+  vector<int> nums3 = {};
+  cout << "Test 3: " << sol.longestConsecutive(nums3) << "\n";
+  // Expected: 0
+
+  vector<int> nums4 = {9};
+  cout << "Test 4: " << sol.longestConsecutive(nums4) << "\n";
+  // Expected: 1
+
+  return 0;
+}
